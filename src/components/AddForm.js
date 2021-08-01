f6a60d9c03e9d1bdec001286609ae76249d45d9c
Mitchell <mitchell.griffith1990@gmail.com>
2021-08-01 16:19:18 -0700

    mvp

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+import { addSmurf, setError } from '../actions';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+    return ({
+        errorMessage: state.error
+    })
+};
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -13,16 +21,17 @@ const AddForm = (props) => {
             ...state,
             [e.target.name]:e.target.value
         });
-    }
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            errorMessage = "Name, position and nickname fields are required.";
+            props.setError();
+        } else {
+            props.addSmurf(state);
         }
-    }
+    };
 
-    const errorMessage = "";
 
     return(<section>
         <h2>Add Smurf</h2>
@@ -44,14 +53,14 @@ const AddForm = (props) => {
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
+                props.errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.errorMessage}</div>
             }
             <button>Submit Smurf</button>
         </form>
     </section>);
 }
 
-export default AddForm;
+export default connect(mapStateToProps, {addSmurf, setError})(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import logger from "redux-logger";
+import reducer from "./reducers";
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store = {store}>
+        <App />, 
+    </Provider>,
     rootElement
 );
 
